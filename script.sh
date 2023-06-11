653b76621a4abb4090a913488f57dc2a7fa47915
quitran <qui.tran@xomad.com>
2023-06-11 18:07:30 +0700

    Test email notification

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 echo "Hello from Mario. This is the first time for studying Jenkins."
 echo "this is the shell script, modified to test github webhook"
-echo "Test email notification3"
+echo "Test email notification4"
