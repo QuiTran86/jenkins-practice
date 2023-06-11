47ca48a5fdf9bbd419ed0366b4a9900187b13b9c
quitran <qui.tran@xomad.com>
2023-06-11 21:41:20 +0700

    Test telegram bot notification

--- script.sh
+++ script.sh
@@ -1,3 +1,4 @@
 echo "Hello from Mario. This is the first time for studying Jenkins."
 echo "this is the shell script, modified to test github webhook"
 echo "Test email notification5"
+echo "Test Telegram bot notification"
